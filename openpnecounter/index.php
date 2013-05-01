01345968f39681272a8792ef2c0f5320fac77f26
手嶋守 <tejima@gmail.com>
2013-05-01 22:22:43 +0900

    to json

--- openpnecounter/index.php
+++ openpnecounter/index.php
@@ -4,16 +4,17 @@ $cachetime = 5 * 60;
 // Serve from the cache if it is younger than $cachetime
 if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile)) {
     include($cachefile);
-    echo "<!-- Cached copy, generated ".date('H:i', filemtime($cachefile))." -->\n";
 }else{
         ob_start(); // Start the output buffer
         /* The code to dynamically generate the page goes here */
         $fp = fopen("http://update.openpne.jp/graph.php", "r");
         $str = fread($fp,5000);
-
         //<p id="total">Total: 4661</p> 
         preg_match('/Total:\\s(.*?)</',$str,$matches);
+
+        echo '{"item":[{"text":"OpenPNE set up","value":"';
         echo $matches[1] ;
+        echo '"},{"text":"","value":""}]}';
 
         // Cache the output to a file
         $fp = fopen($cachefile, 'w');
